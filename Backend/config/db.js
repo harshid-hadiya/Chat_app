c5954cb5d8657de2a3199ab60805cf6114b9a9bb
harshid-hadiya <ahirharshidj@gmail.com>
2025-07-03 18:12:35 +0530

    this is for the change of the connection string

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 function connection() {
-    mongoose.connect(process.env.CONNECTION_STRING).then((res) => {
+    mongoose.connect("mongodb://localhost:27017/chat_app_data").then((res) => {
         console.log("Your database successfully connected: "+res.connection.host);
     }).catch((err) => {
         console.log("There is some error: " + err);
