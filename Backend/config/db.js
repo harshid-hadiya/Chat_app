3c2e24a9e3f55be6c5a014f6bb2a5d31a66808e5
harshid-hadiya <ahirharshidj@gmail.com>
2025-12-19 16:31:12 +0530

    changing the connection string

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 function connection() {
-    mongoose.connect("mongodb://localhost:27017/chat_app_data").then((res) => {
+    mongoose.connect("mongodb+srv://23ithar032:scrRs8p8Dqh8kgUl@mymongo.3lqj4.mongodb.net/Live_chat").then((res) => {
         console.log("Your database successfully connected: "+res.connection.host);
     }).catch((err) => {
         console.log("There is some error: " + err);
